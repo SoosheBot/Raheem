524e3a4034c53a9f5e802e724a02bf789e47a10a
Christopher Hernandez <cerberean661@gmail.com>
2020-03-23 10:30:23 -0700

    basic setup for StopDetails

---
+++ raheem/src/components/forms/StopDetails.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function StopDetails() {
+    return (
+        <div>
+            <h2>How was your stop?</h2>
+            
+            <input type="checkbox" />
+            <p>Positive</p>
+
+            <input type="checkbox" />
+            <p>Negative</p>
+        </div>
+    )
+}
+
+export default StopDetails;
