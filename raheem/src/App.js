8c1e65eee47d6ab135419c815c5fe54f6b2210f5
Christopher Hernandez <cerberean661@gmail.com>
2020-03-23 11:42:32 -0700

    linking StopDetails component to App.js

--- raheem/src/App.js
+++ raheem/src/App.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom';
 
 /* components */
 import Save from './components/buttons/Save';
+import StopDetails from './components/forms/StopDetails';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       {/* routes using react-router-dom */}
       <Route exact path="/">
         <h1>Raheem</h1>
+        <StopDetails />
       </Route>
     </div>
   );
