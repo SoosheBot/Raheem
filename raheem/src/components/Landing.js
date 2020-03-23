82689e70a881b7593b1370788123a54da3c4e22a
Kristin Parrish <kristinparrish@Kristins-MacBook-Pro.local>
2020-03-23 09:50:51 -0600

    landing

---
+++ raheem/src/components/Landing.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+//Purpose is to 
+function Landing() {
+
+    return(
+        <div>
+            <h2>Your story can end police violence.</h2>
+            <p>Report and track police to build safer communities for people of color.</p>
+        </div>
+
+        <div>
+            <h2>About Raheem</h2>
+            <p>Raheem is an independent service for reporting police conduct in the United States.</p>
+        </div>
+    )
+}
+
+export default Landing
+
+
+
+
+
+
