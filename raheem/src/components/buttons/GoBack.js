4d4a6da25f53f40e519ddac3feaac9d5d36f0a66
Kristin Parrish <kristinparrish@Kristins-MacBook-Pro.local>
2020-03-23 13:13:17 -0600

    added go back button

---
+++ raheem/src/components/buttons/GoBack.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../index.css';
+
+function GoBack() {
+    return (
+        <button className="save">
+            Go Back
+        </button>
+    )
+}
+
+export default GoBack;
