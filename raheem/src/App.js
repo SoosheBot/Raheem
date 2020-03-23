5e6ef5efd0bde6d6a57998e8ff455cf7393efe8e
Alex Cooter <56034009+alex-lc@users.noreply.github.com>
2020-03-23 13:35:40 -0500

    Removed temporary routes I was using for viewing the components

--- raheem/src/App.js
+++ raheem/src/App.js
@@ -1,20 +1,12 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 
-/* components */
-import Save from './components/buttons/Save';
-import ThankYou from './components/ThankYou';
-
 function App() {
   return (
     <div>
-
       {/* routes using react-router-dom */}
       <Route exact path="/">
         <h1>Raheem</h1>
-      </Route>
-      <Route path="/thankyou">
-        <ThankYou />
       </Route>
     </div>
   );
