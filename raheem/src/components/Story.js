48da5dae20e83858046334510b3d498d4467692a
Alex Cooter <56034009+alex-lc@users.noreply.github.com>
2020-03-24 14:48:11 -0500

    package-lock.json fix and deleted global and forms folder

--- raheem/src/components/Story.js
+++ raheem/src/components/Story.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-function TextInput() {
+function Story() {
 
     return (
         <div>
@@ -9,4 +9,4 @@ function TextInput() {
     )
 }
 
-export default TextInput;
+export default Story;
