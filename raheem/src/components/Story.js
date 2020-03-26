838ea7e60f052767b91ca583495e4586426076f6
Christopher Hernandez <cerberean661@gmail.com>
2020-03-25 20:18:28 -0700

    setting up yellow form

--- raheem/src/components/Story.js
+++ raheem/src/components/Story.js
@@ -9,29 +9,48 @@ import Save from './buttons/Save';
 
 import styled from "styled-components";
 
-const Form = styled.div`
+const FormY = styled.div`
     margin-left: auto;
     margin-right: auto;
     width: 375px;
     background-color: #FFF600;
 `;
 
-const WhiteBG = styled.div`
+const FormW = styled.form`
     background-color: #FFFFFF;
+    margin: 1em;
 `;
 
 const Heading1 = styled.h1`
-    font-size: 3em;
+    font-size: 4em;
     text-align: center;
-    margin: 1em;
+    padding-right: 1em;
+    padding-left: 1em;
+    padding-top: 1em;
+    padding-bottom: 0.5em;
 `;
 
 const Heading2 = styled.h2`
     text-align: center;
+    padding: 1em;
+`;
+
+const Close = styled.p`
+    content: 'x';
+    font-weight: 300;
+`;
+
+const PHeading = styled.p`
+    text-align: center;
+    font-size: 18px;
+    margin-left: 3em;
+    margin-right: 3em;
+    margin-bottom: 3em;
 `;
 
 const PTag = styled.p`
-    font-size: 16px;
+    font-size: 14px;
+    padding: 2em;
 `;
 
 function Story() {
@@ -42,47 +61,34 @@ function Story() {
     };
 
     return (
-        <Form>
+        <FormY>
             <div>
+                <div>
+                    <Heading2>My Stories</Heading2>
+
+                </div>
+                <Heading1>Your story matters.</Heading1>
+                <PHeading>The more we know about police behavior, the greater chance we have to change it.</PHeading>
+            </div>
+
+            <FormW onSubmit={handleSubmit(onSubmit)}>
                 <PTag>
                     Describe the indicent from start to finish. Be as descriptive as possible, and remember to include details about the officer's attitude and actions during this encounter.
                 </PTag>
 
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <input
-                        name="story"
-                        ref={register({
-                        required: 'Required',
-                        pattern: {
-                            message: "input is required"
-                        }
-                        })}
-                    />
-                    {errors.email && errors.email.message}
-                </form>
+                <input
+                    name="story"
+                    ref={register({
+                    required: 'Required',
+                    pattern: {
+                        message: "input is required"
+                    }
+                    })}
+                />
+                {errors.email && errors.email.message}
+            </FormW>
 
-            </div>
-
-            <div className="tagsButtonContainer">
-                <div>
-                    <p>Click here to proceed.</p>
-                    <Continue />
-                    {/* continue to demographics */}
-                </div>
-                <div>
-                    <p>Click here to go back.</p>
-                    <GoBack />
-                    {/* go back to tags */}
-                </div>
-                <div>
-                    <p>Click here to save your responses and come back later.</p>
-                    <Save />
-                    {/* go to email */}
-                </div>
-            </div>
-
-            {/* progress bar */}
-        </Form>
+        </FormY>
     )
 }
 
