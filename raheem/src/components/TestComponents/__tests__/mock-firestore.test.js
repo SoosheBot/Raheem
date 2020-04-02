ce929baedd917ef2be23f5f72db80ef2a06e9f5c
sooshe <sooshe@sooshebot.com>
2020-04-02 12:35:09 -0500

    add new test --get all records-- to mock-firestore

--- raheem/src/components/TestComponents/__tests__/mock-firestore.test.js
+++ raheem/src/components/TestComponents/__tests__/mock-firestore.test.js
@@ -1,5 +1,9 @@
 const { FakeFirestore } = require("firestore-jest-mock");
-// const { mockCollection } = require('firestore-jest-mock/mocks/firestore');
+const {
+    mockCollection,
+    mockGet,
+    mockWhere,
+  } = require('firestore-jest-mock/mocks/firestore');
 
 const db = new FakeFirestore({
   test: [
@@ -20,7 +24,18 @@ test("it can fetch a single record in a collection", async () => {
   expect(data).toHaveProperty("Lname", "Simpson");
 });
 
-test("adds a new record to the collection", async () => {
+test('It can request all records in collection', async () => {
+    // Assert we are looking at the right collection
+    expect(mockCollection).toHaveBeenCalledWith('test');
+    // Assert that we make no queries
+    expect(mockWhere).not.toHaveBeenCalled();
+    // Assert that we finally request the data
+    expect(mockGet).toHaveBeenCalled();
+  });
+
+
+
+test("it adds a new record to the collection", async () => {
   const record = await db.collection("test").add({
     id: "3d2a",
     Fname: "Krusty",
@@ -30,4 +45,17 @@ test("adds a new record to the collection", async () => {
   const data = record.data();
   expect(data).toHaveProperty("Fname", "Krusty");
   expect(data).toHaveProperty("Lname", "the Clown");
+  expect(data).toHaveProperty("age", 53);
 });
+
+test("it can edit a single record in a collection", async () => {
+    const record = await db
+      .collection("test")
+      .doc("2e3a")
+      .get();
+    expect(record.exists).toBe(true);
+    expect(record.id).toBe("2e3a");
+    const data = record.data();
+    expect(data).toHaveProperty("Fname", "Marge");
+    expect(data).toHaveProperty("Lname", "Simpson");
+  });
