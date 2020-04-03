4c766e8a8a6c0cc886e8aa832d34427ab810e79c
sooshe <sooshe@sooshebot.com>
2020-04-02 21:41:34 -0500

    update add users test -- complete crud operations

--- raheem/src/components/TestComponents/__tests__/firestore.test.js
+++ raheem/src/components/TestComponents/__tests__/firestore.test.js
@@ -1,6 +1,5 @@
 const { FakeFirestore } = require("firestore-jest-mock");
 
-
 const db = new FakeFirestore({
   users: [
     { id: "1abc", Fname: "Homer", Lname: "Simpson", age: 39 },
@@ -21,33 +20,39 @@ test("it can fetch a single record in a collection", async () => {
 });
 
 test("it can fetch an entire collection", () => {
+  //calls the collection "users"
   db
   .collection("users")
   .get()
-  .then(querySnapshot => {
-    expect(querySnapshot.forEach).toBeTruthy();
+  .then(function (querySnapshot) {
     expect(querySnapshot.docs.length).toBe(2);
     querySnapshot.forEach(doc => {
       expect(doc.exists).toBe(true);
-      expect(doc.data()).toBeTruthy();
-    console.log("users data is:", doc.data())
-    })  
-  });
-   
+    console.log("users:", doc.id, " => ", doc.data());
+    })
+  })
+  .catch(err => {
+    console.log("error fetching collection", err)
+  })
 });
 
-test("it adds a new record to the collection", async () => {
-  const record = await db.collection("users").add({
-    id: "3ghi",
-    Fname: "Krusty",
-    Lname: "the Clown",
-    age: 53
-  });
-  expect(record.exists).toBe(true);
-  const data = record.data();
-  expect(data).toHaveProperty("Fname", "Krusty");
-  expect(data).toHaveProperty("Lname", "the Clown");
-  expect(data).toHaveProperty("age", 53);
+test("it adds a new record to the collection", () => {
+  db.collection("users")
+    .add({
+      id: "3ghi",
+      Fname: "Krusty",
+      Lname: "the Clown",
+      age: 53
+    })
+    .then(function (docs) { 
+      const data = docs.data();
+      expect(data).toHaveProperty("Fname", "Krusty");
+      expect(data).toHaveProperty("Lname", "the Clown");
+      expect(data).toHaveProperty("age", 53);
+    })
+    .catch(err => {
+      console.log("error adding to users collection", err);
+    });
 });
 
 test("it deletes a record", () => {
