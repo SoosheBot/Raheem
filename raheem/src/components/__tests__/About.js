19b7dbb2aecdf85e779ce0d4b7a875bc9c8befc3
Alex Cooter <56034009+alex-lc@users.noreply.github.com>
2020-04-09 22:53:28 -0500

    About component tests

--- raheem/src/components/__tests__/About.js
+++ raheem/src/components/__tests__/About.js
@@ -4,16 +4,28 @@ import { render, fireEvent } from '@testing-library/react';
 import About from '../About';
 
 const mockHistoryPush = jest.fn();
+const mockHistoryGoBack = jest.fn();
 
 jest.mock('react-router-dom', () => ({
     ...jest.requireActual('react-router-dom'),
     useHistory: () => ({
-        push: mockHistoryPush
+        push: mockHistoryPush,
+        goBack: mockHistoryGoBack
     })
 }));
 
 describe('<About />', () => {
     it('renders successfully', () => {
         render(<About />);
+    });
+});
+
+describe('Go Back button', () => {
+    it('uses useHistory to take the user back a page', () => {
+        const container = render(<About />);
+        const img = container.getByTestId('goBackButton');
+
+        fireEvent.click(img);
+        expect(mockHistoryGoBack).toHaveBeenCalled();
     });
 });
