1a5918801c10a8a3527c280d8925bfc055bbbb47
KP Parrish <57117452+KParrish193@users.noreply.github.com>
2020-04-16 15:57:18 -0600

    updated about component styling

--- raheem/src/components/About.js
+++ raheem/src/components/About.js
@@ -3,7 +3,7 @@ import { useHistory } from 'react-router-dom';
 
 //styles
 import styled from 'styled-components';
-import { Container, Content, Controls, Divider, Heading, Subheading, HeaderContainer, backButton } from '../styles/global';
+import { PageContainer, Container, YellowHeaderContainer, Content, BackButton, SmallHeading, Heading, Subheading, HeadingContainer, HeaderContainer } from '../styles/global';
 
 /* assets */
 import Back from '../assets/Back.svg';
@@ -19,95 +19,92 @@ function About(props) {
     }
 
     return (
-        <Container className="container">
-
-            <backButton>
-                <div className="go-back">
+        <PageContainer>
+            <HeaderContainer>
+            <BackButton>
                     <img data-testid="goBackButton" onClick={() => history.goBack()} src={Back} alt="Go Back" />
-                </div>
-            </backButton>
-
-            <HeaderContainer>
-                <Heading id="top">
-                    About Raheem
-                </Heading>
+            </BackButton>
             </HeaderContainer>
 
-            <Container className="strategy">
-                <Subheading>
-                    Raheem is an independent service for reporting police conduct in the United States.
-                </Subheading>
+            <Container>
+            <HeadingContainer className="about, page-top">
+                <Heading className="about">
+                About Raheem
+                </Heading>
+            </HeadingContainer>
+            
+            <Subheading className="about">
+                Raheem is an independent service for reporting police conduct in the United States.
+            </Subheading>
 
-                <Content>
-                    We work to translate the lived experiences of people impacted by police violence into effective policy, officer accountability, and new narratives about how we keep our communities safe. Partners in our work include community-run oversight structures, public defenders, and advocacy organizations.
-                </Content>
+            <Content className="about">
+                We work to translate the lived experiences of people impacted by police violence into effective policy, officer accountability, and new narratives about how we keep our communities safe. Partners in our work include community-run oversight structures, public defenders, and advocacy organizations.
+            </Content>
 
-                <Subheading>
-                    Our Story
-                </Subheading>
+            <Subheading className="about">
+                Our Story
+            </Subheading>
 
-                <Content>
-                    Brandon D. Anderson founded Raheem after he lost his life partner to police violence during a routine traffic stop. The officer had a history of being physically abusive, particularly during traffic stops, but no one had ever reported him.
-                </Content>
+            <Content className="about">
+                Brandon D. Anderson founded Raheem after he lost his life partner to police violence during a routine traffic stop. The officer had a history of being physically abusive, particularly during traffic stops, but no one had ever reported him.
+            </Content>
 
-                <Subheading>
-                    Our Strategy
-                </Subheading>
+            <Subheading className="about">
+                Our Strategy
+            </Subheading>
 
-                <Content>
-                    Getting killed by police is the sixth leading cause of death for young Black men in America. Yet, we practically know nothing about how the 63 million police interactions every year shape our lives or the lives of our loved ones—until it's too late. There are 18,000 police departments in the country, each with its own unique, complicated process of reporting police misconduct. As a result, 95% of people do not report police violence and oversight boards miss the opportunity to have these experiences shape their policy agenda. We're working to change that.
-                </Content>
-                <Content>
-                    We engage communities who’ve been directly impacted by policing to hear their stories about officer conduct that usually go unreported. Then we use this information to help local, independent investigators hold police officers accountable and support community-led oversight structures in advancing policies to end police violence.
-                </Content>
-                <Content>
-                    Raheem uses data to identify places with the highest rates of police violence in the country. Then we partner with community oversight structures in these areas to collect firsthand reports of police conduct and help people file formal complaints that can lead to officers being held accountable. Our digital outreach and organizing strategy enables us to reach people who’ve been recently impacted by policing and connect them to oversight boards, organizations, and advocacy campaigns that can help them seek justice and accountability for police violence.
-                </Content>
-            </Container>
+            <Content className="about">
+                Getting killed by police is the sixth leading cause of death for young Black men in America. Yet, we practically know nothing about how the 63 million police interactions every year shape our lives or the lives of our loved ones—until it's too late. There are 18,000 police departments in the country, each with its own unique, complicated process of reporting police misconduct. As a result, 95% of people do not report police violence and oversight boards miss the opportunity to have these experiences shape their policy agenda. We're working to change that.
+            </Content>
+            <Content className="about">
+                We engage communities who’ve been directly impacted by policing to hear their stories about officer conduct that usually go unreported. Then we use this information to help local, independent investigators hold police officers accountable and support community-led oversight structures in advancing policies to end police violence.
+            </Content>
+            <Content className="about">
+                Raheem uses data to identify places with the highest rates of police violence in the country. Then we partner with community oversight structures in these areas to collect firsthand reports of police conduct and help people file formal complaints that can lead to officers being held accountable. Our digital outreach and organizing strategy enables us to reach people who’ve been recently impacted by policing and connect them to oversight boards, organizations, and advocacy campaigns that can help them seek justice and accountability for police violence.
+            </Content>
 
-            <HeaderContainer>
-                <Heading>
+            <HeadingContainer className="about">
+                <h2>
                     Our Team
-                </Heading>
-            </HeaderContainer>
+                </h2>
+            </HeadingContainer>
 
-            <Container className="team">
-                <h4>
+                <SmallHeading className="about">
                     Brandon Anderson
-                </h4>
-                <Content>
+                </SmallHeading>
+                <Content className="about">
                     Brandon D. Anderson, Founder & CEO, founded Raheem after police killed his partner during a routine traffic stop. He is a U.S. army veteran, 2019 TED Fellow, and Smithsonian Ingenuity Award Nominee. Brandon graduated from Georgetown University in 2015.
                 </Content>
 
-                <h4>
+                <SmallHeading className="about">
                     Nadav Savio
-                </h4>
-                <Content>
+                </SmallHeading>
+                <Content className="about">
                     Nadav Savio, Head of Product at Raheem, has over 20 years of experience including leading product design for Google Search and user experience for Google.org. He previously ran his own design firm.
                 </Content>
 
-                <h4>
+                <SmallHeading className="about">
                     Samuel Sinyangwe
-                </h4>
-                <Content>
+                </SmallHeading>
+                <Content className="about">
                     Samuel Sinyangwe, Data Strategist at Raheem, is a policy analyst who founded Mapping Police Violence, the first national database of police killings in the U.S. Samuel graduated from Stanford University in 2012.
                 </Content>
 
-                <h4>
+                <SmallHeading className="about">
                     Ariel Matos
-                </h4>
-                <Content>
+                </SmallHeading>
+                <Content className="about">
                     Ariel Matos, Growth Strategist at Raheem, ran content strategy for FADER and Refinery29 (where she increased revenue over $100 million). Ariel graduated from University of San Francisco in 2013.
                 </Content>
 
-                <h4>
+                <SmallHeading className="about">
                     Ellie Dehghan
-                </h4>
-                <Content>
+                </SmallHeading>
+                <Content className="about">
                     Ellie Dehghan has worked with communities surviving violence and seeking justice through a variety of pathways. She’s currently the Sr. Director of Legal at Callisto, a nonprofit utilizing tech to combat sexual assault & advance justice. Raised in the East Bay by Mexican and Iranian immigrants, Ellie is graduate of Stanford Law School, and UC Berkeley.
                 </Content>
 
-                <Content>
+                <Content className="about">
                     Proudly based in Oakland, CA, Raheem is a nonprofit, 501(c)(3) organization operating nationwide for a world in which we all feel safe to live and love freely.
                 </Content>
 
@@ -116,9 +113,7 @@ function About(props) {
             {/* <ButtonContainer className="landingButtonContainer">
                 <a href="#top" className="top">Back To Top</a>
             </ButtonContainer> */}
-
-
-        </Container>
+        </PageContainer>
     )
 }
 
