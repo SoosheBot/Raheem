04bd011472ac6a988b715ccc2c4fd7aa1fb466f3
sooshe <sooshe@sooshebot.com>
2020-04-19 19:33:53 -0500

    move firebase scripts to head in index.html

--- raheem/src/config/firebase.js
+++ raheem/src/config/firebase.js
@@ -1,20 +1,7 @@
 import firebase from 'firebase/app';
-import 'firebase/firebase-auth'
 import 'firebase/database';
 import 'firebase/firestore';
 
-
-
-// var firebaseConfig = {
-//   apiKey: "AIzaSyCO6v_oMVa9lwKmiaT4VAI3NGj8GeCXtY4",
-//   authDomain: "raheem-mercy.firebaseapp.com",
-//   databaseURL: "https://raheem-mercy.firebaseio.com",
-//   projectId: "raheem-mercy",
-//   storageBucket: "raheem-mercy.appspot.com",
-//   messagingSenderId: "29941601473",
-//   appId: "1:29941601473:web:dec5b326bec24a1c50f040",
-//   measurementId: "G-3H9BJ0D7SR"
-// };
 
 var firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
@@ -22,8 +9,6 @@ var firebaseConfig = {
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASEURL,
   projectId: "raheem-mercy",
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
-
-  // may not be using the following, commenting it out if so
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
   appId: process.env.REACT_APP_FIREBASE_APPID,
   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID
