448383443aabd631d32d448646d065a2f3063d4b
Alex Cooter <56034009+alex-lc@users.noreply.github.com>
2020-04-19 23:18:01 -0500

    mobile structure of officer dashboard started

--- raheem/src/components/Dashboard.js
+++ raheem/src/components/Dashboard.js
@@ -1,9 +1,54 @@
 import React from 'react';
 
+/* styles */
+import { PageContainer, Container, Content, BackButton, SmallHeading, Heading, Subheading, HeadingContainer, HeaderContainer } from '../styles/global';
+import { DashboardOfficer, DashboardView, DashboardTitle, DashboardSearch, DashboardMainTitle, DashboardTagSearch } from '../styles/dashboard';
+
 export default function Dashboard() {
     return (
-        <div>
+        <PageContainer>
+            <DashboardOfficer>
+                <DashboardMainTitle>
+                    <h2>Officer Name</h2>
+                </DashboardMainTitle>
+                <div className="db-officer-info">
+                    <div className="db-officer-img">
+                        <div className="img"></div>
+                    </div>
 
-        </div>
+                    <div className="db-officer-info">
+                        <div className="placeholder"></div>
+                        <div className="placeholder"></div>
+                        <div className="placeholder"></div>
+                        <div className="placeholder"></div>
+                        <div className="placeholder"></div>
+                    </div>
+                </div>
+            </DashboardOfficer>
+
+            <DashboardView>
+                <div className="title-container">
+                    <DashboardTitle>Stories</DashboardTitle>
+                    <DashboardTitle>Stats</DashboardTitle>
+                    <DashboardTitle>Map</DashboardTitle>
+                </div>
+
+                <DashboardSearch>
+                    <input
+                        type="text"
+                        name="query"
+                        autoComplete="off"
+                    />
+                </DashboardSearch>
+
+                <DashboardTagSearch>
+                    <input
+                        type="text"
+                        name="tagQuery"
+                        autoComplete="off"
+                    />
+                </DashboardTagSearch>
+            </DashboardView>
+        </PageContainer>
     )
 }
