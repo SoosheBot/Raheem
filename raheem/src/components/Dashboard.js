0e147a3a2baeb9ca0cf9d9156765ca99ff603563
Alex Cooter <56034009+alex-lc@users.noreply.github.com>
2020-04-19 23:21:18 -0500

    starting on separate dashboard components

--- raheem/src/components/Dashboard.js
+++ raheem/src/components/Dashboard.js
@@ -32,22 +32,6 @@ export default function Dashboard() {
                     <DashboardTitle>Stats</DashboardTitle>
                     <DashboardTitle>Map</DashboardTitle>
                 </div>
-
-                <DashboardSearch>
-                    <input
-                        type="text"
-                        name="query"
-                        autoComplete="off"
-                    />
-                </DashboardSearch>
-
-                <DashboardTagSearch>
-                    <input
-                        type="text"
-                        name="tagQuery"
-                        autoComplete="off"
-                    />
-                </DashboardTagSearch>
             </DashboardView>
         </PageContainer>
     )
