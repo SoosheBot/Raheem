22802a9e802d17a110cca2c2a3d9cffad3a15610
sooshe <sooshe@sooshebot.com>
2020-04-22 16:39:39 -0500

    update code with comments

--- raheem/src/components/Dashboard/Map.js
+++ raheem/src/components/Dashboard/Map.js
@@ -5,64 +5,42 @@ import styled from "styled-components";
 
 const TOKEN = process.env.REACT_APP_MAPBOX_ACCESSTOKEN;
 
-const geolocateStyle = {
-  float: "left",
-  margin: "50px",
-  padding: "10px",
-};
-
 export default function Map() {
   const [viewport, setViewport] = useState({
     width: "100vh",
     height: "100vh",
     latitude: 37,
     longitude: -112,
-    zoom: 10
+    zoom: 10,
   });
 
-//   const location = () => {
-//     GeolocateControl.trigger()
-//   };
-  const _onViewportChange = (viewport) =>
-    setViewport({ ...viewport});
+  //refactored for DRYness in return
+  const _onViewportChange = (viewport) => setViewport({ ...viewport });
 
-   
-  // useEffect(() => {
-  //   }, []);
-
-  
-  
   return (
     <div>
       {/* <StartEnd>Map Start</StartEnd> */}
-
-    
 
       <ReactMapGL
         className="MapBox"
         {...viewport}
         mapboxApiAccessToken={TOKEN}
         // mapStyle can be changed to another map style at https://www.mapbox.com/gallery/
+        // changed mapstyle for visualizing it more easily in test -- can change back in prod
         mapStyle="mapbox://styles/mapbox/streets-v11"
         onViewportChange={_onViewportChange}
         latitude={0}
         logitude={0}
         // onLoad={location}
       >
-
-
+        {/* built-in functionality for geolocation in react-map-gl */}
         <GeolocateControl
-          // style={geolocateStyle}
-          
           positionOptions={{ enableHighAccuracy: true }}
           trackUserLocation={true}
           onViewportChange={_onViewportChange}
-
         />
-        
       </ReactMapGL>
 
-      
       {/* <StartEnd>Map End</StartEnd> */}
     </div>
   );
