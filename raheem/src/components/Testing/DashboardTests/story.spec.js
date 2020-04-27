0e013226252906185dcac9da3cc0c56ab5523d8d
sooshe <sooshe@sooshebot.com>
2020-04-26 21:51:15 -0500

    update story.spec

--- raheem/src/components/Testing/DashboardTests/story.spec.js
+++ raheem/src/components/Testing/DashboardTests/story.spec.js
@@ -2,6 +2,7 @@ import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 
 import Story from '../../Dashboard/Story';
+import { StoryContainer, StoryContent, LongStoryContent, StoryTagContainer, StoryTag } from '../../../styles/dashboard';
 
 describe("Story page", () => {
     it("renders", () => {
